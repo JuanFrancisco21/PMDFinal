206dc0248fb02e5d771eaeef91c7275dad366e55
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2022-03-01 18:14:42 +0100

    Completado Creacion de nuevas obras

--- src/app/Pages/Work/addwork/addwork.page.ts
+++ src/app/Pages/Work/addwork/addwork.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Work } from 'src/app/Model/work';
 import { Worker } from 'src/app/Model/worker';
 import { WorkService } from 'src/app/Services/work.service';
@@ -12,41 +13,63 @@ import { WorkerService } from 'src/app/Services/worker.service';
 })
 export class AddworkPage implements OnInit {
   workForm: FormGroup | any;
-  Work: Work|any;
-  worker: any[] = [];
+  Work: Work | any;
+  workers: any[] = [];
 
-  constructor(private fb: FormBuilder, 
-    private WorkerS: WorkerService, 
-    private WorkS: WorkService) {
+  constructor(private fb: FormBuilder,
+    private WorkerS: WorkerService,
+    private WorkS: WorkService,
+    private router: Router) {
 
   }
 
-  ngOnInit(): void {
+  /**
+   * Consulta de todos los trabajadores.
+   */
+  async ionViewDidEnter() {
+    this.workers = await this.WorkerS.getAllWorkers();
+  }
+
+  /**
+   * Creación del validador del fomulario a rellenar.
+   */
+  ngOnInit(){
     this.workForm = this.fb.group({
-      id: ['-1', this.workForm.required],
-      name: ['', this.workForm.required],
-      description: ['', this.workForm.required],
-      location: ['', this.workForm.required],
-      chief: ['', this.workForm.required],
-      workers: [ '', this.workForm.required]
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      chief: ['', Validators.required]
     });
   }
 
+  /**
+   * Método de creación de obra en la BBDD
+   */
   onSubmit() {
     this.Work = this.saveWork();
     this.WorkS.createObra(this.Work);
     this.workForm.reset();
+    this.goBack();
   }
 
+  /**
+   * Método que devuelve work con los datos introducidos.
+   * @returns obra con nombre, descripcion y encargado(Worker).
+   */
   saveWork() {
     const saveWork = {
       name: this.workForm.get('name').value,
       description: this.workForm.get('description').value,
-      location: [this.workForm.get('x').value,
-                this.workForm.get('y').value],
       chief: this.workForm.get('chief').value,
-      workers: this.workForm.get('workers').value,
     };
     return saveWork;
   }
+
+  /**
+   * Método para cambiar de vista a la principal
+   */
+  goBack() {
+    this.router.navigate(['/main/tabs/tab1'])
+  }
+
+
 }
