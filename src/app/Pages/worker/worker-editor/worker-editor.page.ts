ea2325bbcb8d885915685a95f0c21246b3d8d067
AngelRabasco <rabascoangel01@gmail.com>
2022-05-25 14:26:00 +0200

    Added user dailylog export

--- src/app/Pages/worker/worker-editor/worker-editor.page.ts
+++ src/app/Pages/worker/worker-editor/worker-editor.page.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Worker } from 'src/app/Model/worker';
+import { DailylogService } from 'src/app/Services/dailylog.service';
+import { ExcelService } from 'src/app/Services/excel.service';
 
 @Component({
   selector: 'app-worker-editor',
@@ -9,10 +11,30 @@ import { Worker } from 'src/app/Model/worker';
 })
 export class WorkerEditorPage implements OnInit {
   @Input() worker: Worker;
+  private logs: any[] = [];
 
-  constructor(private modalController: ModalController) { }
+  constructor(private modalController: ModalController, private dailyLogService: DailylogService, private excelService: ExcelService) { }
 
   ngOnInit() {
+    this.loadLogs();
+  }
+
+  private async loadLogs() {
+    await this.dailyLogService.getLogsByWorker(this.worker.id).then(res => {
+      res.forEach(element => {
+        this.logs.push({
+          'Nombre y apellidos': element.workerWork.worker.name + ' ' + element.workerWork.worker.surname,
+          'Obra': element.workerWork.work.name,
+          'Fecha': element.date,
+          'Horas': element.hours + ' horas',
+        });
+      });
+      console.log(this.logs);
+    });
+  }
+
+  exportExcel() {
+    this.excelService.exportAsExcelFile(this.logs, this.worker.name + ' ' + this.worker.surname + '.xlsx');
   }
 
   public async closeEditor() {
